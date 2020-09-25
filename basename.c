27928a442f0ed2bce41440a619b3f7d8f3e8ed4c
HUNcamper <selimnahimi@gmail.com>
2020-09-25 09:25:31 +0000

    basename

--- basename.c
+++ basename.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+void printNumber(int number) {
+    printf("%d\n", number);
+}
+
 int main() {
-    printf("%d", 5);
+    int num = 15;
+    printNumber(num);
+    for (int i = 0; i < 100; i++)
+    {
+        printf("%d. Macska\n", i+1);
+    }
+    
     return 0;
 }
+
+// -t kapcsolo: teszt esetek fajlnev atiras helyett
